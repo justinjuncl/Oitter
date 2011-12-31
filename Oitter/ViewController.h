dc4597b5a26b4351f95740405f1e76aa2e675c38
Justin Jun <J@Justin-Juns-MacBook-Pro.local>
2011-12-31 23:10:53 +0900

    Initial Commit

---
+++ Oitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Oitter
+//
+//  Created by Justin Jun on 12/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
